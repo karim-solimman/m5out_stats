e93f63eaeb1d5ddc6ead793b0ec44eb65429dff0
Karim Soliman <karim.soliman@pua.edu.eg>
2023-07-29 13:57:12 +0800

    Update d_remove.py

--- d_remove.py
+++ d_remove.py
@@ -3,28 +3,37 @@ triBA27_nodes = [
     '211', '212', '213', '221', '222', '223', '231', '232', '233',
     '311', '312', '313', '321', '322', '323', '331', '332', '333'
 ]
-triBA27_shortest_path = {
-    '111':0, '112':0, '113':0, '121':0, '122':0, '123':0, '131':0, '132':0, '133':0,
-    '211':0, '212':0, '213':0, '221':0, '222':0, '223':0, '231':0, '232':0, '233':0,
-    '311':0, '312':0, '313':0, '321':0, '322':0, '323':0, '331':0, '332':0, '333':0
-}
+triBA27_shortest_path = dict()
+
+def construct_triba_dictionary():
+    for src in triBA27_nodes:
+        triBA27_shortest_path[src] = dict()
+        for dst in triBA27_nodes:
+            if src == dst:
+                continue
+            triBA27_shortest_path[src][dst] = 0
 
 if __name__ == "__main__":
     m5_file = open("/home/soliman/gem5_dremove/gem5/routing_output.txt")
+    construct_triba_dictionary()
+    for line in m5_file:
+        tmp_line = line.split()
+        src = tmp_line[3]
+        dst = tmp_line[5]
+        if src == dst:
+            continue
+        path = []
+        index = 7
+        while tmp_line[index] != "-":
+            path.append(tmp_line[index])
+            index += 1
+        if triBA27_shortest_path[src][dst] == 0:
+            triBA27_shortest_path[src][dst] = len(path)
+        elif triBA27_shortest_path[src][dst] > len(path):
+            triBA27_shortest_path[src][dst] = len(path)
     for src in triBA27_nodes:
         for dst in triBA27_nodes:
             if src == dst:
                 continue
-            for line in m5_file:
-                path = []
-                tmp_line = line.split()
-                tmp_src = tmp_line[3]
-                tmp_dst = tmp_line[5]
-                if tmp_src == src and tmp_dst == dst:
-                    index = 7
-                    while tmp_line[index] != "-":
-                        path.append(tmp_line[index])
-                        index += 1
-                    if triBA27_shortest_path[]
+            print(f"{src} ==> {dst} = {triBA27_shortest_path[src][dst]}")
     m5_file.close()
-    # print(f"Count = {count} - count_lines = {count_lines} == {count/count_lines*100}")
