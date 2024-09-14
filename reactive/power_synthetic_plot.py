a6fa7424e6c12dc1e36c1572291064a928de9868
Karim Soliman <karim.soliman@pua.edu.eg>
2024-09-14 16:55:00 +0800

    Create power_synthetic_plot.py

---
+++ reactive/power_synthetic_plot.py
@@ -0,0 +1,16 @@
+import openpyxl
+
+routing_algorithms = [
+    "ddra",
+    "dm4t",
+    "dm4t-randomEqual",
+    "dm4t-randomEqual-modified",
+    "full-adaptive",
+    "full-adaptive-randomEqual",
+    "semi-adaptive",
+    "semi-adaptive-randomEqual",
+    "semi-adaptive-randomEqual-modified"
+]
+
+if __name__ == "__main__":
+    
